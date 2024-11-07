1aa3468ef5bb29f94a651e15332a1fc579bca698
Christopherjfc <christopherjfc1@gmail.com>
2024-11-07 19:03:34 +0100

    En esta actividad creé un método que retorna un keyPair que utiliza una clave con algoritmoRSA, una método que cifra con algoritmo RSA utilizando la clave pública y un método que descifra con algoritmo RSA utilizando la clave privada

---
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,47 @@
+package iticbcn.xifratge;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+public class ClauPublica {
+    public KeyPair generaParellClausRSA() throws Exception{
+        // genero una clave utilizando el algoritmo "RSA"
+        KeyPairGenerator clau = KeyPairGenerator.getInstance("RSA");
+
+        // especifico el tamaño de la llave en bits
+        clau.initialize(2048);
+
+        // genero el KeyPair y lo retorno
+        KeyPair keyPair = clau.generateKeyPair();
+        return keyPair;
+    }
+    
+    public byte[] xifraRSA(String msg, PublicKey clauPublica)throws Exception{
+        // especifico que tipo de algoritmo utilizaré
+        Cipher cipher = Cipher.getInstance("RSA");
+
+        // inicializo el cipher en modo encriptado pasandole también la clauPublica
+        cipher.init(Cipher.ENCRYPT_MODE, clauPublica);
+
+        // Le paso el texto que quiero encriptar, esto me devolderá como resultado el texto encriptado en bytes
+        return cipher.doFinal(msg.getBytes());
+    }
+
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey clauPrivada) throws Exception{
+        // especifico que tipo de algoritmo utilizaré
+        Cipher cipher = Cipher.getInstance("RSA");
+
+        // inicializo el cipher en modo desencriptado pasandole también la clauPrivada
+        cipher.init(Cipher.DECRYPT_MODE, clauPrivada);
+
+        // Le paso el texto que quiero desencriptar
+        byte[] desxifraText = cipher.doFinal(msgXifrat);
+
+        // retorno el descifrado ya leíble
+        return new String(desxifraText, "UTF-8");
+    }
+}
